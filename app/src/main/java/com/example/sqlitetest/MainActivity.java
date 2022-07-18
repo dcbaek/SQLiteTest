46d120818b945f2dca21a0a459084b0946e40142
dcbaek <ehdco8988@gmail.com>
2022-07-18 15:34:21 +0900

    SQLiteTest Success

--- app/src/main/java/com/example/sqlitetest/MainActivity.java
+++ app/src/main/java/com/example/sqlitetest/MainActivity.java
@@ -2,23 +2,82 @@ package com.example.sqlitetest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
     myDBHelper myDBHelper;
     EditText edtName, edtNumber, edtNameResultm, edtNumberResult;
-    Button btnInit, btnIsert, btnSelect;
+    Button btnInit, btnInsert, btnSelect;
     SQLiteDatabase sqlDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("구매 리스트 관리");
+
+        edtName = (EditText) findViewById(R.id.edtName);
+        edtNumber = (EditText) findViewById(R.id.edtNumber);
+        edtNameResultm = (EditText) findViewById(R.id.edtNameResult);
+        edtNumberResult = (EditText) findViewById(R.id.edtNumberResult);
+
+        btnInit = (Button) findViewById(R.id.btnInit);
+        btnInsert = (Button) findViewById(R.id.btnInsert);
+        btnSelect = (Button) findViewById(R.id.btnSelect);
+//  초기화를 클릭했을 때 동작하는 리스너, 입력을 클릭하면 editText 값이 입력되는 리스너, 마지막으로 조회를 클릭 할 때
+//  테이블에 입력된 내용이 모두 아래쪽 editText에 출력되는 리스너를 코딩합니다.
+
+        myDBHelper = new myDBHelper(this);
+        btnInit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sqlDB = myDBHelper.getWritableDatabase();
+                myDBHelper.onUpgrade(sqlDB,1,2);
+                sqlDB.close();
+            }
+        });
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("WrongConstant")
+            @Override
+            public void onClick(View view) {
+                sqlDB = myDBHelper.getWritableDatabase();
+                sqlDB.execSQL("INSERT INTO groupTBL VALUES ( '" + edtName.getText().toString() + "', "
+                         + edtNumber.getText().toString() + ");");
+                sqlDB.close();
+                Toast.makeText(getApplicationContext(), "입력됨", 0).show();
+            }
+        });
+
+        btnSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sqlDB = myDBHelper.getReadableDatabase();
+                Cursor cursor;
+                cursor = sqlDB.rawQuery("SELECT * FROM groupTBL;", null);
+
+                String strNames = "목록 리스트" + "\r\n" + "\r\n";
+                String strNumbers = "수량" + "\r\n" + "\r\n";
+
+                while (cursor.moveToNext()) {
+                    strNames += cursor.getString(0) + "\r\n";
+                    strNumbers += cursor.getString(1) + "\r\n";
+                }
+                edtNameResultm.setText(strNames);
+                edtNumberResult.setText(strNumbers);
+                cursor.close();
+                sqlDB.close();
+            }
+        });
+        // 이때 myDBHelper 인스턴트를 생성합니다 myDBHelper()생성자가 실행 되어 groupDB 파일이 생성됩니다.
     }
 }
